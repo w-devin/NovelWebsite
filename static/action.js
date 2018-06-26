349f296dca393d985a2bbed523b8fd0a3c2795f2
UjnBin <YUE1051969307@163.com>
2018-06-26 15:39:53 +0800

    change the frontend page(index.html) - binyou

--- static/action.js
+++ static/action.js
@@ -26,14 +26,5 @@
           });
         })
       ;
-      $('.ui.container .ui.menu a.item')
-        .on('click', function() {
-          $(this)
-            .addClass('active')
-            .siblings()
-            .removeClass('active')
-          ;
-        })
-      ;
     })
   ;
