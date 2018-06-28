17725276e2de7819fc88c4d2d65b6b6fa42b0551
Hanchen1997 <498813962@qq.coxm>
2018-06-28 21:12:49 +0800

    徐翰琛上传的倒数第二个版本！ -hanc

--- author/author.py
+++ author/author.py
@@ -8,6 +8,7 @@
 from flask import Flask, render_template, request,session, url_for, redirect
 from author.auth import bp as auth_bp
 from author.book import bp as book_bp
+from utils.db_utils import show_books
 
 app = Flask(__name__)
 app.register_blueprint(auth_bp)
@@ -20,7 +21,8 @@ def index():
     if session.get('author_id') is None:
         return redirect(url_for('auth.login'))
     if request.method == 'GET':
-        return render_template('index.html')
+        rows = show_books(session.get('author_id'))
+        return render_template('index.html',books = rows)
 
 
 if __name__ == '__main__':
--- author/book.py
+++ author/book.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 
 from utils.db_utils import Admin, Author, Reader,create_book,Book,update_link,show_books,author_select_book,update_bookmes
@@ -12,21 +12,22 @@ bp = Blueprint('book', __name__, url_prefix='/book')
 @bp.route('/updatebook', methods=('GET', 'POST'))
 def updatebook():
     if request.method == 'POST':
-        rows = author_select_book(int(session.get('main_book')))[0][1]
-        return render_template('book/update.html',book = rows)
+        row = author_select_book(int(request.form['bookid']))[0]
+        return render_template("book/Personal.html", book=row)
+
 
 @bp.route('/updatebookbyid', methods=('GET', 'POST'))
 def updatebookbyid():
     if request.method == 'POST':
         message = [request.form['bookname'],request.form['bookdescription'],request.form['catalog'],request.form['currentstate']]
         update_bookmes(int(request.form['bookid']),message)
-        rows = show_books(session.get('author_id'))
-        return render_template("index.html", books=rows)
+        return jsonify({"code":200, "result": '修改成功'})
 
 @bp.route('/addchapter', methods=('GET', 'POST'))
 def addchapter():
     if request.method == 'POST':
-        return render_template('book/chapter.html')
+        bookid = int(request.form['bookid'])
+        return render_template('book/chapter.html',bookid = bookid)
 
 @bp.route('/addChapter', methods=('GET', 'POST'))
 def addChapter():
@@ -34,7 +35,7 @@ def addChapter():
         id = int(request.form['bookid'])
         print(request.form['title'])
         print(request.form['text'])
-        return render_template('book/chapter.html')
+        return jsonify({"code": 200, "result": '添加成功'})
 
 
 @bp.route('/createbook', methods=('GET', 'POST'))
@@ -51,8 +52,7 @@ def createbook():
                 book = Book.selectBy(bookName=request.form['bookname'], author=session.get('author_name'))[0]
                 link = '/book/' + str(book.id) + '/'
                 update_link(link, book.id)
-                rows = show_books(session.get('author_id'))
-                return render_template("index.html", books=rows)
+                return jsonify({"code": 200, "result": '修改成功'})
             flash(error)
         error = 'bookname is repeat'
         return redirect(url_for('book.createbook'))
